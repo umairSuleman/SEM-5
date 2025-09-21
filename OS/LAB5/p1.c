bfdc0b1e50674cd58046d5e2857696621a178b16
umairSuleman <umair.mitmpl2023@learner.manipal.edu>
2025-09-21 15:29:18 +0530

    Update p1.c

--- OS/LAB5/p1.c
+++ OS/LAB5/p1.c
@@ -1,1 +1,22 @@
+#include <sys/types.h>    //for types like pid_t
+#include <sys/wait.h>     //for wait function
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>        //stands for UNIX standard, has all the process control functions
 
+int main(){    //always stick to int main() instead of void
+    int pid = fork();
+    int status;
+    switch(pid){
+        case -1 :    //fork failure case
+            perror("fork failed");
+            exit(1);
+        case 0 :    //pid=0 when in child
+            printf("Child running\n");
+            break;
+        default :   //pid is non zero when in parent
+            wait(&status);
+            printf("Parent running : Child completed with status %d\n", status);
+    }
+    return 0;
+}
